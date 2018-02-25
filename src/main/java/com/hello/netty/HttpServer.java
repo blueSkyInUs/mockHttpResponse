89a0abbe514d5f59bca1383cff065a7cc33a51c7
lesson <Lesson.lai@bluepay.asia>
2018-02-25 23:10:32 +0800

    过滤掉默认的ico

--- src/main/java/com/hello/netty/HttpServer.java
+++ src/main/java/com/hello/netty/HttpServer.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,7 +38,7 @@ public class HttpServer implements ApplicationListener {
             String port=System.getProperties().getProperty("project.port","7777");
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
-                    .channel(DefaultNioSocketChannel.class)
+                    .channel(NioServerSocketChannel.class)
                     .option(ChannelOption.SO_BACKLOG, 1024)
                     .childHandler(httpChannelInitService).option(ChannelOption.SO_BACKLOG, 128)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
